c1af8019f100b6e4372b3bc88a98982efb5d7de3
Rafe7778 <rafek7778@gmail.com>
2026-09-09 10:04:50 +0000

    line added from dev branch

--- sample.sh
+++ sample.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+x#!/bin/bash
 echo "today date is " $date
 echo "my name is rafe and yours zoheb"
 echo "zoheb bkb"
 echo "add forth line"
 echo "new line added from github
+echo "new line added fromn dev"
